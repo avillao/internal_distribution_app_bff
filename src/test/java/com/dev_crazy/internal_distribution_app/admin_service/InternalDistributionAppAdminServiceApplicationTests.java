551afa9f0fb5b328195f0878bbcec777fef0921f
Aaron Villao <60727026+avillao@users.noreply.github.com>
2026-08-25 11:13:23 -0500

    first commit

---
+++ src/test/java/com/dev_crazy/internal_distribution_app/admin_service/InternalDistributionAppAdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev_crazy.internal_distribution_app.admin_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalDistributionAppAdminServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
