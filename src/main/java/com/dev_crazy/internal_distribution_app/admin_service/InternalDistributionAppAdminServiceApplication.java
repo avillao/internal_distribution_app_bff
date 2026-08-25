db5303cbb7adf65d8a95409d66a0652def49a19c
Aaron Villao <60727026+avillao@users.noreply.github.com>
2026-08-25 11:03:59 -0500

    first commit

---
+++ src/main/java/com/dev_crazy/internal_distribution_app/admin_service/InternalDistributionAppAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dev_crazy.internal_distribution_app.admin_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalDistributionAppAdminServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternalDistributionAppAdminServiceApplication.class, args);
+    }
+
+}
